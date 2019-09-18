d8fe1b36116d89df8be9a92417320a4a4c233110
danijel <danijel.kecman@gmail.com>
2019-09-18 18:28:19 +0200

    - init

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Danijel Kecman on 9/18/19.
+//  Copyright © 2019 Danijel Kecman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
